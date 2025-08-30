40759aa535799d1fb007d9d79f66c01c54735938
Othmane <oelouajdi@hotmail.com>
2025-08-31 01:18:42 +0200

    refactor: mise à jour de HairbookApplication

--- src/main/java/com/hairbook/HairbookApplication.java
+++ src/main/java/com/hairbook/HairbookApplication.java
@@ -1,9 +1,12 @@
 package com.hairbook;
 
+import com.hairbook.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class HairbookApplication {
 
 	public static void main(String[] args) {
