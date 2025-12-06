9150ff24100fd106d7bb03490c570909a7c9d69d
othmane <oelouajdi@hotmail.com>
2025-12-06 12:54:24 +0100

    Ajout du DTO RedeemPointsRequest pour l’échange de points de fidélité

---
+++ src/main/java/com/hairbook/dto/RedeemPointsRequest.java
@@ -0,0 +1,24 @@
+package com.hairbook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Requête pour échanger des points de fidélité contre un service gratuit.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RedeemPointsRequest {
+
+    /** Identifiant du service choisi pour la récompense */
+    private UUID serviceId;
+
+    /** Nombre de points à échanger (généralement 1000) */
+    private Integer pointsToRedeem;
+}
