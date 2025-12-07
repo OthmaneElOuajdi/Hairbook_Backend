5000b318f24ebcd966f88ca0c15dd7f189121481
othmane <oelouajdi@hotmail.com>
2025-12-07 20:30:08 +0100

    Ajout du DTO RefundRequestDTO

---
+++ src/main/java/com/hairbook/dto/RefundRequestDTO.java
@@ -0,0 +1,34 @@
+package com.hairbook.dto;
+
+
+import com.hairbook.entity.RefundRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefundRequestDTO {
+    private String id;
+    private String appointmentId;
+    private String userId;
+    private String userName;
+    private String userEmail;
+    private String appointmentDate;
+    private String serviceName;
+    private Double amount;
+    private String reason;
+    private String justificationFileName;
+    private String justificationFileUrl;
+    private RefundRequest.RefundStatus status;
+    private String adminComment;
+    private String processedBy;
+    private LocalDateTime processedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
